e65b248e222fecc0fcfa60a4b44515392791450f
Paul Frank Pacheco Carpio <paul.frankpc@gmail.com>
2018-10-26 16:42:51 -0500

    Initial Commit

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Paul Frank Pacheco Carpio on 10/26/18.
+//  Copyright © 2018 Paul Frank Pacheco Carpio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
